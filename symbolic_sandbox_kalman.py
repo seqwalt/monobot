c3924ac8929a7e0bf14d395ad863f2be6b5ab5f4
sequoyah <sequoyah.n.walters@gmail.com>
2023-09-23 11:22:38 -0500

    sympy kalman work

--- symbolic_sandbox_kalman.py
+++ symbolic_sandbox_kalman.py
@@ -5,23 +5,24 @@ import sympy as sp
 # Constants
 r, bl, dt = sp.symbols('r bl dt') # wheel radius, base length btw wheels, delta time
 x_0, y_0 = sp.symbols('x_0 y_0')    # position of tag 0 (origin)
+yaw_i_world = sp.symbols('yaw_i_world') # yaw from tag i to world frame
 
-# State vector (length 15)
-x, y, yaw = sp.symbols('x y yaw')    # robot position and yaw
+# State vector (length 17)
+x, y, yaw, v, w= sp.symbols('x y yaw v w')    # robot position, yaw, speed, yaw rate
 bw_r, bw_l = sp.symbols('bw_r bw_l') # bias of wheel angular velocities
 x_1, y_1 = sp.symbols('x_1 y_1')    # position estimate of tag 1
 x_2, y_2 = sp.symbols('x_2 y_2')    # position estimate of tag 2
 x_3, y_3 = sp.symbols('x_3 y_3')    # position estimate of tag 3
 x_4, y_4 = sp.symbols('x_4 y_4')    # position estimate of tag 4
 x_5, y_5 = sp.symbols('x_5 y_5')    # position estimate of tag 5
-X = sp.Matrix([x, y, yaw, bw_l, bw_r, x_1, y_1, x_2, y_2, x_3, y_3, x_4, y_4, x_5, y_5])
+X = sp.Matrix([x, y, yaw, v, w, bw_l, bw_r, x_1, y_1, x_2, y_2, x_3, y_3, x_4, y_4, x_5, y_5])
 #sp.pprint(X)
 
 # Process noise
-nx, ny, nyaw = sp.symbols('nx ny nyaw')
+nx, ny, nyaw, nv, nw = sp.symbols('nx ny nyaw nv nw')
 nbw_l, nbw_r = sp.symbols('nbw_l nbw_r')
-ProcNoise = sp.Matrix([nx, ny, nyaw, nbw_l, nbw_r, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) # Note tag positions are not used in the Kalman propogation step
-N = sp.Matrix([nx, ny, nyaw, nbw_l, nbw_r])
+ProcNoise = sp.Matrix([nx, ny, nyaw, nv, nw, nbw_l, nbw_r, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) # Note tag positions are not used in the Kalman propogation step
+N = sp.Matrix([nx, ny, nyaw, nv, nw, nbw_l, nbw_r])
 #sp.pprint(N)
 
 # Measurement noise
@@ -41,30 +42,26 @@ U = sp.Matrix([w_r, w_l])
 #sp.pprint(U)
 
 # Discretized dynamics
-F = ProcNoise + X + dt * sp.Matrix([ (r/2)*(w_r - bw_r + w_l - bw_l)*sp.cos(yaw),
-                                     (r/2)*(w_r - bw_r + w_l - bw_l)*sp.sin(yaw),
-                                     (r/bl)*(w_r - bw_r - (w_l - bw_l)),
-                                     0, 0,
-                                     0, 0,
-                                     0, 0,
-                                     0, 0,
-                                     0, 0,
-                                     0, 0 ])
+F = ProcNoise + sp.Matrix([ x + dt*v*sp.cos(yaw),
+                            y + dt*v*sp.sin(yaw),
+                            yaw + dt*w),
+                            (r/2)*(w_r - bw_r + w_l - bw_l),
+                            (r/bl)*(w_r - bw_r - (w_l - bw_l)),
+                            bw_l, bw_r,
+                            x_1, y_1,
+                            x_2, y_2,
+                            x_3, y_3,
+                            x_4, y_4,
+                            x_5, y_5, ])
 #sp.pprint(F)
 
-# Measurement dynamics
-h = MeasNoise + sp.Matrix([ sp.cos(yaw)*(x_0 - x) + sp.sin(yaw)*(y_0 - y),
-                            sp.cos(yaw)*(y_0 - y) - sp.sin(yaw)*(x_0 - x),
-                            sp.cos(yaw)*(x_1 - x) + sp.sin(yaw)*(y_1 - y),
-                            sp.cos(yaw)*(y_1 - y) - sp.sin(yaw)*(x_1 - x),
-                            sp.cos(yaw)*(x_2 - x) + sp.sin(yaw)*(y_2 - y),
-                            sp.cos(yaw)*(y_2 - y) - sp.sin(yaw)*(x_2 - x),
-                            sp.cos(yaw)*(x_3 - x) + sp.sin(yaw)*(y_3 - y),
-                            sp.cos(yaw)*(y_3 - y) - sp.sin(yaw)*(x_3 - x),
-                            sp.cos(yaw)*(x_4 - x) + sp.sin(yaw)*(y_4 - y),
-                            sp.cos(yaw)*(y_4 - y) - sp.sin(yaw)*(x_4 - x),
-                            sp.cos(yaw)*(x_5 - x) + sp.sin(yaw)*(y_5 - y),
-                            sp.cos(yaw)*(y_5 - y) - sp.sin(yaw)*(x_5 - x),])
+# Measurement dynamics for tag i
+h_i = MeasNoise + sp.Matrix([ sp.cos(yaw)*(x_i - x) + sp.sin(yaw)*(y_i - y),
+                             sp.cos(yaw)*(y_i - y) - sp.sin(yaw)*(x_i - x),
+                             yaw_i_world - yaw,
+                             w*(sp.cos(yaw)*(y_i - y) - sp.sin(yaw)*(x_i - x)) - v,
+                             -w*(sp.cos(yaw)*(x_i - x) + sp.sin(yaw)*(y_i - y)),
+                             -w ])
 #sp.pprint(h)
 
 # ------- Linearization ------- #
