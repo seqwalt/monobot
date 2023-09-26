4cbff57a3c8452c55d894b5310813c72965d1c7b
monobot <monobot@example.com>
2023-09-26 17:30:04 -0500

    test_stream update

--- fiducial_detect.py
+++ fiducial_detect.py
@@ -6,8 +6,8 @@ from dt_apriltags import Detector
 class TagDetect:
     def __init__(self):
         self.detector = Detector(families='tag36h11',
-                                 nthreads=1,
-                                 quad_decimate=1.0,
+                                 nthreads=2,
+                                 quad_decimate=2.0,
                                  quad_sigma=0.0,
                                  refine_edges=1,
                                  decode_sharpening=0.25,
