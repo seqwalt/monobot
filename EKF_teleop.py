2ba76f46e9319e822ddae00f50760e92a8a24d89
monobot <monobot@example.com>
2023-09-28 05:58:26 -0500

    speed for ekf teleop

--- EKF_teleop.py
+++ EKF_teleop.py
@@ -141,7 +141,7 @@ try:
         X_est = EKF.GetEKFState()
 
         # Update control input (user input)
-        speed = 0.0
+        speed = 0.25
         if (not yaw_rate == kp.yaw_rate):
             yaw_rate = kp.yaw_rate
             if (not kp.command == None):
